a0230bbcbfb2832b36ca1446ab95eb28ec93ff5e
nikhily13 <nikhilyeola995@gmail.com>
2022-10-07 19:07:26 +0530

    new file

---
+++ src/timepass/demo.java
@@ -0,0 +1,19 @@
+package timepass;
+
+public class demo 
+{
+   public static void main(String[] args) {
+	
+	   
+	   System.out.println("nikhil yeola");
+	   
+	   System.out.println(" i have a total 3.2 years of experience in manual as well as automation testing");
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+}
+}
