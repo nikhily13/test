9987bc0be5114e6ad5427d0975af9c6e414d2b9b
nikhily13 <nikhilyeola995@gmail.com>
2022-10-15 18:08:56 +0530

    nikhil folder is added

---
+++ src/timepass/Nikhil.java
@@ -0,0 +1,24 @@
+package timepass;
+
+public class Nikhil 
+{
+    
+    public static void main(String[] args) {
+		
+    	int a=100;
+    	int b=200;
+    	
+    	System.out.println(a+b);
+    	
+    	System.out.println(a-b);
+    	
+    	System.out.println(a/b);
+    	
+    	System.out.println(a*b);
+    	
+   
+	}
+    
+    
+    
+}
