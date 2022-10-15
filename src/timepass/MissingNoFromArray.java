d46f74e10d94d8fe4f7c7ef43b2beb509bf20117
nikhily13 <nikhilyeola995@gmail.com>
2022-10-15 17:53:09 +0530

    software testing add

---
+++ src/timepass/MissingNoFromArray.java
@@ -0,0 +1,43 @@
+package timepass;
+
+public class MissingNoFromArray 
+{
+
+	public static void main(String[] args) {
+		
+	// initilize array
+/*	
+	int arr[]= {1,2,3,4,5,6,7,9};  //n=7
+	 int n=arr.length+1;
+
+	int sum=(n*(n+1))/2	  ; // 28
+		  
+	for(int i=0; i<arr.length; i++)
+	{
+		sum=sum-arr[i];  // 27,25,22,18,12,5
+	}
+		  
+	System.out.println("missing no is ="+sum);
+	
+	*/
+	
+		
+		String a= "kunal shirke"; // string declared
+		
+		System.out.println(a.replace("k", "K"));
+		
+		System.out.println(a.replace("kunal", "KUNAL"));
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+}
+}
--- src/timepass/demo.java
+++ src/timepass/demo.java
@@ -7,7 +7,8 @@ public class demo
 	   
 	   System.out.println("nikhil yeola");
 	   
-	   System.out.println(" i have a total 3.2 years of experience in manual as well as automation testing");
+	   System.out.println(" i have a total 3.2 years "
+	   		+ "of experience in manual as well as automation testing");
 	   
 	   
 	   
